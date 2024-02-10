0dccf7e86e3b6d8cce1e56b6c737663570d41945
Lucien Cartier-Tilet <lucien@phundrak.com>
2024-02-10 18:30:09 +0100

    feat: add app version in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,14 +1,22 @@
 <template>
-  <footer class="flex-row flex-spread card">
-    <div id="copyright" class="small">Copyright &copy; {{ currentYear }} Lucien Cartier-Tilet</div>
-    <div id="source">
-      <a class="highlight small" href="https://labs.phundrak.com/phundrak/gege-jdr">Source code</a>
+  <footer class="flex-col flex-spread card gap-1rem">
+    <div class="flex-row flex-spread">
+      <div id="version" class="small">version {{ version }}</div>
+      <div id="copyright" class="small">
+        Copyright &copy; {{ currentYear }} Lucien Cartier-Tilet
+      </div>
+      <div id="source">
+        <a class="highlight small" href="https://labs.phundrak.com/phundrak/gege-jdr"
+          >Source code</a
+        >
+      </div>
     </div>
   </footer>
 </template>
 
 <script setup lang="ts">
 const currentYear = new Date().getFullYear();
+const version = __APP_VERSION__;
 </script>
 
 <style scoped="" lang="less">
